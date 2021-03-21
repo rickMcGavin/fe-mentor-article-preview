e4d9c047bed0c25885df1ea5610c2c0e2271785d
Rick <richard.mcgavin@gmail.com>
2021-03-21 00:17:45 -0400

    Initial setup

---
+++ styles/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+  colors:{
+    primary: {
+      700: 'hsl(217, 19%, 35%)',
+      500: 'hsl(214, 17%, 51%)',
+      300: 'hsl(212, 23%, 69%)',
+      100: 'hsl(210, 46%, 95%)'
+    }
+  },
+  breakpoint: `(min-width: 768px)`
+};
